be74ec10249bb2cb1f97e134cf3ca8a2ce201e32
Illya Sokolov <illya.sokolov82@gmail.com>
2019-10-23 16:38:35 +0300

    Prepare_to_HW0

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -6,6 +6,8 @@ package ru.javawebinar.topjava;
  *
  * @link http://caloriesmng.herokuapp.com/
  * @link https://github.com/JavaOPs/topjava
+ * @see <a href="http://topjava.herokuapp.com">Demo</a>
+ * @see <a href="https://github.com/JavaOPs/topjava">Initial project</a>
  */
 public class Main {
     public static void main(String[] args) {
