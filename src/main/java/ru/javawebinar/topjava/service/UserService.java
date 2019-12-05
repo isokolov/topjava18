6c40add124ee757e234a1d4faa4a878e28d3b9ff
Illya Sokolov <illya.sokolov82@gmail.com>
2019-12-05 16:12:17 +0200

    2.5-2.7 Spring injections

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
@@ -9,9 +11,21 @@ import java.util.List;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFound;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
+@Service
 public class UserService {
 
-    private UserRepository repository;
+    //@Autowired
+    private final UserRepository repository;
+
+    // Constructor injection, also final for UserRepository repository
+    @Autowired
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    /*public void setRepository(UserRepository repository) {
+        this.repository = repository;
+    }*/
 
     public User create(User user) {
         return repository.save(user);
